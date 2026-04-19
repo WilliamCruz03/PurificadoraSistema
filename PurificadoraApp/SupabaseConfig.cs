b179f2f117edacbadfdcf23ef2b58684773c5257
William Crow <153472489+WilliamCruz03@users.noreply.github.com>
2026-04-18 20:49:33 -0600

    Update SupabaseConfig.cs
    Agrega modelos, servicios, vistas y ViewModels

--- PurificadoraApp/SupabaseConfig.cs
+++ PurificadoraApp/SupabaseConfig.cs
@@ -10,6 +10,6 @@ namespace PurificadoraApp
     {
         // Credenciales de Supabase
         public const string Url = "https://usvaqrzptecadesphtlk.supabase.co";
-        public const string AnonKey = "sb_publishable_KWtvWO8ji6DnYFcpticvcg_Tx3vns8x";
+        public const string AnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdmFxcnpwdGVjYWRlc3BodGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NDk4OTQsImV4cCI6MjA5MjEyNTg5NH0.s7s4hoZ0l3aE91VrYqq01yWQKwVhSIgTBD0RzqFPb6I";
     }
 }
